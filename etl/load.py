e6d02fd6859f60c8337ba5d059e57422002ee83a
Odilbek <odilbekmarimov70@gmail.com>
2025-12-17 17:43:51 +0500

    Update load.py

--- etl/load.py
+++ etl/load.py
@@ -1,1 +1,14 @@
+import os
+import pandas as pd
+
+def load_users(df: pd.DataFrame, output_path: str):
+    """
+    Loads transformed data into processed layer.
+    """
+    print("Loading data...")
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    df.to_csv(output_path, index=False)
+
+    print(f"Data successfully written to {output_path}")
 
